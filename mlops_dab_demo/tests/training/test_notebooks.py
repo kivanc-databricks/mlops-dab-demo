17689abf2b4bbd5526cea37d99e55a91811ddf04
kivanc-databricks <kivanc.urgancioglu@databricks.com>
2023-11-07 14:44:25 +0100

    test pull request

--- mlops_dab_demo/tests/training/test_notebooks.py
+++ mlops_dab_demo/tests/training/test_notebooks.py
@@ -1,6 +1,5 @@
 import pathlib
 
-#test pull
 def test_notebook_format():
     # Verify that all Databricks notebooks have the required header
     paths = list(pathlib.Path("./notebooks").glob("**/*.py"))
